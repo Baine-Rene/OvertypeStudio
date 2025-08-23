e8e7f8a979fea8eb37d6025c288caa6535577e4e
Rene Baine <renebaine13@gmail.com>
2025-08-23 00:31:05 -0400

    added animated hero section with framer motion

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,11 @@
 import { Header1 } from "./home/Header";
+import { Hero5 } from "./home/hero";
 
 export default function Home() {
   return (
-       <Header1/>
+       <>
+       <Header1 />
+       <Hero5 />
+       </>
   );
 }
