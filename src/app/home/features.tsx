b871c4244013d1176d0e399bf585ffa4ef576ca8
Rene Baine <renebaine13@gmail.com>
2025-08-25 12:51:43 -0400

    added images for features section

--- src/app/home/features.tsx
+++ src/app/home/features.tsx
@@ -11,53 +11,68 @@ export const Feature = () => (
           </div>
           <div className="flex gap-2 flex-col">
             <h2 className="text-3xl md:text-5xl tracking-tighter max-w-xl font-semibold text-left">
-             What&apos;s new ?
+              What&apos;s new ?
             </h2>
-            <p className="text-lg max-w-xl lg:max-w-lg leading-relaxed tracking-tight text-muted-foreground  text-left">
-              Here are some projects that I&apos;ve on this year
+            <p className="text-lg max-w-xl lg:max-w-lg leading-relaxed tracking-tight text-muted-foreground text-left">
+              Here are some projects that I&apos;ve worked on this year
             </p>
           </div>
         </div>
+
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
-          <div className="bg-muted rounded-md h-full lg:col-span-2 p-6 aspect-square lg:aspect-auto flex justify-between flex-col">
-            <User className="w-8 h-8 stroke-1" />
+          <div
+            className="rounded-md h-full lg:col-span-2 p-6 aspect-square lg:aspect-auto flex flex-col justify-between"
+            style={{
+              backgroundImage: "url('/images/features/image-1.png')",
+              backgroundSize: "cover",
+              backgroundPosition: "center",
+            }}
+          >
+            <User className="w-8 h-8 stroke-1 text-white" />
             <div className="flex flex-col">
-              <h3 className="text-xl tracking-tight">Pay supplier invoices</h3>
-              <p className="text-muted-foreground max-w-xs text-base">
-                Our goal is to streamline SMB trade, making it easier and faster
-                than ever.
-              </p>
-            </div>
-          </div>
-          <div className="bg-muted rounded-md  aspect-square p-6 flex justify-between flex-col">
-            <User className="w-8 h-8 stroke-1" />
-            <div className="flex flex-col">
-              <h3 className="text-xl tracking-tight">Pay supplier invoices</h3>
-              <p className="text-muted-foreground max-w-xs text-base">
-                Our goal is to streamline SMB trade, making it easier and faster
-                than ever.
-              </p>
+             
             </div>
           </div>
 
-          <div className="bg-muted rounded-md aspect-square p-6 flex justify-between flex-col">
-            <User className="w-8 h-8 stroke-1" />
-            <div className="flex flex-col">
-              <h3 className="text-xl tracking-tight">Pay supplier invoices</h3>
-              <p className="text-muted-foreground max-w-xs text-base">
-                Our goal is to streamline SMB trade, making it easier and faster
-                than ever.
-              </p>
+          <div
+            className="rounded-md aspect-square p-6 flex flex-col justify-between"
+            style={{
+              backgroundImage: "url('/images/features/image-2.png')",
+              backgroundSize: "cover",
+              backgroundPosition: "center",
+            }}
+          >
+            <User className="w-8 h-8 stroke-1 text-white" />
+            <div>
+              
             </div>
           </div>
-          <div className="bg-muted rounded-md h-full lg:col-span-2 p-6 aspect-square lg:aspect-auto flex justify-between flex-col">
-            <User className="w-8 h-8 stroke-1" />
-            <div className="flex flex-col">
-              <h3 className="text-xl tracking-tight">Pay supplier invoices</h3>
-              <p className="text-muted-foreground max-w-xs text-base">
-                Our goal is to streamline SMB trade, making it easier and faster
-                than ever.
-              </p>
+
+          <div
+            className="rounded-md aspect-square p-6 flex flex-col justify-between"
+            style={{
+              backgroundImage: "url('/images/features/image-3.png')",
+              backgroundSize: "cover",
+              backgroundPosition: "center",
+            }}
+          >
+            <User className="w-8 h-8 stroke-1 text-white" />
+            <div>
+             
+            </div>
+          </div>
+
+          <div
+            className="rounded-md h-full lg:col-span-2 p-6 aspect-square lg:aspect-auto flex flex-col justify-between"
+            style={{
+              backgroundImage: "url('/images/features/image-4.png')",
+              backgroundSize: "cover",
+              backgroundPosition: "center",
+            }}
+          >
+            <User className="w-8 h-8 stroke-1 text-white" />
+            <div>
+              
             </div>
           </div>
         </div>
